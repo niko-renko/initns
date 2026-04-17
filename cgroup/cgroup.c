0a96b9780ba2935f0774a335602153b97ff44a1f
null <null>
2026-04-17 14:35:54 +0000

    fix cleaning up cgroups

--- cgroup/cgroup.c
+++ cgroup/cgroup.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <poll.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
@@ -46,19 +47,36 @@ int new_cgroup(char *name) {
     return fd;
 }
 
-static void rm_poll(char *path) {
+static void wait_cgroup_empty(const char *cgpath) {
+    char events_path[PATH_MAX];
+    snprintf(events_path, sizeof(events_path), "%s/cgroup.events", cgpath);
+    int fd = open(events_path, O_RDONLY);
+    if (fd < 0)
+        die("cgroup.events open");
+
     for (;;) {
-        if (rmdir(path) == 0)
+        char buf[256];
+        if (lseek(fd, 0, SEEK_SET) < 0)
+            die("cgroup.events lseek");
+        ssize_t n = read(fd, buf, sizeof(buf) - 1);
+        if (n < 0)
+            die("cgroup.events read");
+        buf[n] = '\0';
+
+        char *p = strstr(buf, "populated ");
+        if (!p)
+            die("cgroup.events parse");
+        if (p[strlen("populated ")] == '0')
             break;
-        if (errno == EBUSY)
-            continue;
-        else
-            break;
-        usleep(1000);
+
+        struct pollfd pfd = {.fd = fd, .events = POLLPRI};
+        if (poll(&pfd, 1, -1) < 0 && errno != EINTR)
+            die("poll cgroup.events");
     }
+    close(fd);
 }
 
-static void rm_cgroup_internal(char *path) {
+static void rm_cgroup_children(char *path) {
     DIR *dir = opendir(path);
     if (!dir)
         die("opendir");
@@ -76,12 +94,12 @@ static void rm_cgroup_internal(char *path) {
             die("lstat");
 
         if (S_ISDIR(st.st_mode)) {
-            rm_cgroup_internal(child);
-            rm_poll(child);
+            rm_cgroup_children(child);
+            if (rmdir(child) == -1)
+                die("rmdir");
         }
     }
 
-    rm_poll(path);
     closedir(dir);
 }
 
@@ -89,7 +107,10 @@ void rm_cgroup(char *name) {
     char cgpath[PATH_MAX];
     snprintf(cgpath, sizeof(cgpath), "%s/%s/%s", CGROUP_ROOT, CGROUP_NAME,
              name);
-    rm_cgroup_internal(cgpath);
+    wait_cgroup_empty(cgpath);
+    rm_cgroup_children(cgpath);
+    if (rmdir(cgpath) == -1)
+        die("rmdir");
 }
 
 void set_frozen_cgroup(char *name, int frozen) {
